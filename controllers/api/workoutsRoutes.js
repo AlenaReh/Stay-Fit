1d7e30e7b7ddf42d6f93d42bdd33fb8c30eed068
Alena Rehberger <alenarehberger92@gmail.com>
2021-10-26 22:32:38 -0500

    Started working on routes

---
+++ controllers/api/workoutsRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const db = require("../../models");
+
+//get route for your last workout
+
+//post route for adding a new workout
+
+//put router by id for update
+
+//get a 'range' of workouts
+
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+//route for "/"
+
+//route for your dashboard (stats)
+
+//route for a new workout
+
+
+module.exports = router;
